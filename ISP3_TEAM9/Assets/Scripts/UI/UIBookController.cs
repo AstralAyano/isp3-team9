f5269620c481addd66b7f465c0ce235078101a94
Bernard Ng <bernardngboonhao@gmail.com>
2023-08-16 22:06:19 +0800

    Finished Inventory UI (Not func), started on Status UI
    - Still need to make Inventory Bar UI and link to BookUI Inventory's Hot Bar.
    
    - Still need to make Artefact slots work.

--- ISP3_TEAM9/Assets/Scripts/UI/UIBookController.cs
+++ ISP3_TEAM9/Assets/Scripts/UI/UIBookController.cs
@@ -1,18 +1,201 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEditor.ShaderKeywordFilter;
 
 public class UIBookController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("References")]
+    [SerializeField] private Animator animController;
+    [SerializeField] private Button[] rightButtons;
+    [SerializeField] private Button[] leftButtons;
+    [SerializeField] private GameObject[] rightIcons;
+
+    [Header("Pages")]
+    [SerializeField] private GameObject statusPage;
+    [SerializeField] private GameObject inventoryPage;
+    [SerializeField] private GameObject skillPage;
+    [SerializeField] private GameObject settingPage;
+
+    [Header("Sprites")]
+    [SerializeField] private Sprite[] bookmark;
+
+    [Header("Variables")]
+    [SerializeField] private string currPage = "Status";
+    [SerializeField] private Vector2 buttonStartPos;
+    [SerializeField] private Vector2[] buttonCurrPos;
+    [SerializeField] private Vector2 iconStartPos;
+    [SerializeField] private Vector2[] iconCurrPos;
+
+    private float xOffset = 30;
+
     void Start()
     {
-        
+        animController = GetComponentInChildren<Animator>();
+
+        rightButtons[0].image.sprite = bookmark[0];
+
+        for (int i = 1; i < rightButtons.Length; i++)
+        {
+            rightButtons[i].image.sprite = bookmark[1];
+            leftButtons[i - 1].gameObject.SetActive(false);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void BookButtons(int page_type)
     {
-        
+        statusPage.SetActive(false);
+        inventoryPage.SetActive(false);
+        skillPage.SetActive(false);
+        settingPage.SetActive(false);
+
+        int page = page_type / 10;
+        int type = page_type % 10;
+
+        switch (page)
+        {
+            case 1:
+                currPage = "Status";
+                break;
+            case 2:
+                currPage = "Inventory";
+                break;
+            case 3:
+                currPage = "Skill";
+                break;
+            case 4:
+                currPage = "Setting";
+                break;
+        }
+
+        StartCoroutine(AnimationStart(type));
+    }
+
+    private IEnumerator AnimationStart(int dir)
+    {
+        HideAllButtons(true);
+
+        for (int i = 0; i < rightButtons.Length; i++)
+        {
+            buttonCurrPos[i] = rightButtons[i].GetComponent<RectTransform>().anchoredPosition;
+            buttonCurrPos[i].Set(buttonStartPos.x, buttonCurrPos[i].y);
+            rightButtons[i].gameObject.GetComponent<RectTransform>().anchoredPosition = buttonCurrPos[i];
+
+            iconCurrPos[i] = rightIcons[i].GetComponent<RectTransform>().anchoredPosition;
+            iconCurrPos[i].Set(iconStartPos.x, iconCurrPos[i].y);
+            rightIcons[i].GetComponent<RectTransform>().anchoredPosition = iconCurrPos[i];
+        }
+
+        switch (dir)
+        {
+            case 0:
+                animController.SetTrigger("flipLeft");
+                break;
+            case 1:
+                animController.SetTrigger("flipRight");
+                break;
+        }
+
+        switch (currPage)
+        {
+            case "Status":
+                rightButtons[0].gameObject.transform.localPosition -= new Vector3(xOffset, 0, 0);
+                rightIcons[0].transform.localPosition += new Vector3(27.5f, 0, 0);
+
+                yield return new WaitForSeconds(1);
+
+                ShowAllButtons(false);
+
+                rightButtons[0].image.sprite = bookmark[0];
+                rightButtons[1].image.sprite = bookmark[1];
+                rightButtons[2].image.sprite = bookmark[1];
+                rightButtons[3].image.sprite = bookmark[1];
+                
+                statusPage.SetActive(true);
+                break;
+            case "Inventory":
+                rightButtons[1].gameObject.transform.localPosition -= new Vector3(xOffset, 0, 0);
+                rightIcons[1].transform.localPosition += new Vector3(27.5f, 0, 0);
+
+                yield return new WaitForSeconds(1);
+
+                ShowAllButtons(false);
+
+                rightButtons[0].gameObject.SetActive(false);
+                rightButtons[1].image.sprite = bookmark[0];
+                rightButtons[2].image.sprite = bookmark[1];
+                rightButtons[3].image.sprite = bookmark[1];
+
+                leftButtons[0].gameObject.SetActive(true);
+
+                inventoryPage.SetActive(true);
+                break;
+            case "Skill":
+                rightButtons[2].gameObject.transform.localPosition -= new Vector3(xOffset, 0, 0);
+                rightIcons[2].transform.localPosition += new Vector3(27.5f, 0, 0);
+                
+                yield return new WaitForSeconds(1);
+
+                ShowAllButtons(false);
+
+                rightButtons[0].gameObject.SetActive(false);
+                rightButtons[1].gameObject.SetActive(false);
+                rightButtons[2].image.sprite = bookmark[0];
+                rightButtons[3].image.sprite = bookmark[1];
+
+                leftButtons[0].gameObject.SetActive(true);
+                leftButtons[1].gameObject.SetActive(true);
+
+                skillPage.SetActive(true);
+                break;
+            case "Setting":
+                rightButtons[3].gameObject.transform.localPosition -= new Vector3(xOffset, 0, 0);
+                rightIcons[3].transform.localPosition += new Vector3(27.5f, 0, 0);
+
+                yield return new WaitForSeconds(1);
+
+                ShowAllButtons(false);
+
+                rightButtons[0].gameObject.SetActive(false);
+                rightButtons[1].gameObject.SetActive(false);
+                rightButtons[2].gameObject.SetActive(false);
+                rightButtons[3].image.sprite = bookmark[0];
+
+                leftButtons[0].gameObject.SetActive(true);
+                leftButtons[1].gameObject.SetActive(true);
+                leftButtons[2].gameObject.SetActive(true);
+
+                settingPage.SetActive(true);
+                break;
+        }
+    }
+
+    void HideAllButtons(bool includeLeft)
+    {
+        for (int i = 0; i < rightButtons.Length; i++)
+        {
+            rightButtons[i].gameObject.SetActive(false);
+
+            if (includeLeft && i < leftButtons.Length)
+            {
+                leftButtons[i].gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void ShowAllButtons(bool includeLeft)
+    {
+        for (int i = 0; i < rightButtons.Length; i++)
+        {
+            rightButtons[i].gameObject.SetActive(true);
+
+            if (includeLeft && i < leftButtons.Length)
+            {
+                leftButtons[i].gameObject.SetActive(false);
+            }
+        }
     }
 }
