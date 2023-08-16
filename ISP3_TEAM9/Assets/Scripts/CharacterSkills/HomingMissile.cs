9d0e3de08c79d007bbba92dd80da4d333d264af3
Nitritious <robbiesohwork@gmail.com>
2023-08-16 10:38:24 +0800

    pushing in arrows
    haw yea

---
+++ ISP3_TEAM9/Assets/Scripts/CharacterSkills/HomingMissile.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class HomingMissile : MonoBehaviour
+{
+    public Transform Target;
+
+    public float speed = 5f;
+    public float rotateSpeed = 200f;
+
+    public GameObject explosionEffect;
+
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 direction = (Vector2)Target.position - rb.position;
+
+        direction.Normalize();
+
+        float rotateAmount = Vector3.Cross(direction, transform.up).z;
+
+        rb.angularVelocity = -rotateAmount * rotateSpeed;
+
+        rb.velocity = transform.up * speed;
+    }
+
+    void OnTriggerEnter2D()
+    {
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
