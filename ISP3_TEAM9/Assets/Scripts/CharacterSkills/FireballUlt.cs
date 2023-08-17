95842c9cb800b78b4507d99ae5d217565cf71321
Nitritious <robbiesohwork@gmail.com>
2023-08-17 15:23:00 +0800

    Push fireball

--- ISP3_TEAM9/Assets/Scripts/CharacterSkills/FireballUlt.cs
+++ ISP3_TEAM9/Assets/Scripts/CharacterSkills/FireballUlt.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class FireballUlt : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private SpriteRenderer sr;
 
     private float angleInRad;
     private Vector2 dir;
 
-    private float LastEF;
+    private float LastEF = 0.0f;
+    private bool IsExploded;
+    private bool ActivateTimer = false;
+
+    [SerializeField]
+    private GameObject secondCollider;
+
+    [SerializeField]
+    private GameObject FireBallGFX;
 
     // Start is called before the first frame update
     void Start()
@@ -24,23 +33,45 @@ public class FireballUlt : MonoBehaviour
     void Update()
     {
         rb.AddForce(dir * 0.1f, ForceMode2D.Impulse);
+        if (ActivateTimer)
+        {
+            LastEF += Time.deltaTime;
+            if (LastEF > 5f)
+            {
+                Debug.Log("die fire part 2");
+                Destroy(gameObject);
+            }
+        }
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Enemy") || other.gameObject.CompareTag("Wall"))
         {
-            if (GameObject.FindWithTag("FireBall2ndRadius") && GetComponentInChildren<CircleCollider2D>())
-            {
-                GetComponentInChildren<CircleCollider2D>().gameObject.SetActive(true);
-            }
+            gameObject.transform.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+            IsExploded = true;
+            secondCollider.SetActive(true);
+            FireBallGFX.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0f);
+            ActivateTimer = true;
+            Debug.Log("die fire part 1");
+        }
+    }
 
-            LastEF += Time.deltaTime;
-            if (LastEF >= 5)
-            {
-                Destroy(gameObject);
-            }
-            
+    private void FireBallAOE(Collider2D other)
+    {
+        if (IsExploded)
+        {
+            //do the if gameobject.comparetag<"enemy">
+            //if enemy, set a timer for enemy to dmg over time
+        }
+
+    }
+    private void NotInAOE(Collider2D other)
+    {
+        if (IsExploded)
+        {
+            //stop dmg over time LLLLLL
         }
     }
 }
