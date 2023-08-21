4e2a41152188b97e4e33a26e99ee44aeeab8f4d9
notJY <123806420+notJY@users.noreply.github.com>
2023-08-21 14:25:20 +0800

    Update ScriptablePlayerStats.cs
    why so many classes

--- ISP3_TEAM9/Assets/Scripts/Player/ScriptablePlayerStats.cs
+++ ISP3_TEAM9/Assets/Scripts/Player/ScriptablePlayerStats.cs
@@ -2,34 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerBaseStats
-{
-    public int health;
-    public int maxHealth;
-    public int attack;
-    public int defense;
-    public float attackInterval; //Attack Speed in seconds
-    public int moveSpeed;
-    public int projectileSpeed; //For mage and archer
-}
-
 public class playerStats
 {
     public int health;
     public int maxHealth;
     public int attack;
     public int defense;
-    public float attackInterval; //Attack Speed in seconds
-    public int moveSpeed;
-    public int projectileSpeed; //For mage and archer
-}
-
-public class playerStatPoints
-{
-    public int health;
-    public int defense;
-    public int attackPower;
-    public int attackSpeed;
+    public float attackInterval; //(float)(attackSpeed/10) + 1. For timers in code
+    public int attackSpeed; //(int)(attackInterval - 1) * 10. For display
     public int moveSpeed;
     public int projectileSpeed; //For mage and archer
 }
@@ -56,18 +36,18 @@ public class ScriptablePlayerStats : ScriptableObject
     }
 
     public playerClass chosenClass; //Change this to change class
-    public playerBaseStats chosenBaseStats;
+    public playerStats chosenBaseStats;
     public playerStats chosenStats;
-    public playerStatPoints chosenStatPoints;
+    public playerStats chosenStatPoints;
     public playerStatMultipliers chosenStatMultipliers;
 
     //Dictionaries to store all class stats
-    public Dictionary<playerClass, playerBaseStats> baseStats = new Dictionary<playerClass, playerBaseStats>()
+    public Dictionary<playerClass, playerStats> baseStats = new Dictionary<playerClass, playerStats>()
     {
-        { playerClass.Barbarian, new playerBaseStats{ maxHealth = 80, attack = 10, defense = 2, attackInterval = 0.5f, moveSpeed = 4, projectileSpeed = 0} },
-        { playerClass.Mage, new playerBaseStats{ maxHealth = 60, attack = 15, defense = 1, attackInterval = 1.5f, moveSpeed = 6, projectileSpeed = 4} },
-        { playerClass.Archer, new playerBaseStats{ maxHealth = 60, attack = 10, defense = 1, attackInterval = 1f, moveSpeed = 7, projectileSpeed = 5} },
-        { playerClass.Paladin, new playerBaseStats{ maxHealth = 100, attack = 7, defense = 4, attackInterval = 0.5f, moveSpeed = 5, projectileSpeed = 0} }
+        { playerClass.Barbarian, new playerStats{ maxHealth = 80, attack = 10, defense = 2, attackInterval = 0.5f, moveSpeed = 4, projectileSpeed = 0} },
+        { playerClass.Mage, new playerStats{ maxHealth = 60, attack = 15, defense = 1, attackInterval = 1.5f, moveSpeed = 6, projectileSpeed = 4} },
+        { playerClass.Archer, new playerStats{ maxHealth = 60, attack = 10, defense = 1, attackInterval = 1f, moveSpeed = 7, projectileSpeed = 5} },
+        { playerClass.Paladin, new playerStats{ maxHealth = 100, attack = 7, defense = 4, attackInterval = 0.5f, moveSpeed = 5, projectileSpeed = 0} }
     };
 
     public Dictionary<playerClass, playerStats> currentStats = new Dictionary<playerClass, playerStats>()
@@ -78,20 +58,20 @@ public class ScriptablePlayerStats : ScriptableObject
         { playerClass.Paladin, new playerStats{ health = 100, maxHealth = 100, attack = 7, defense = 4, attackInterval = 0.5f, moveSpeed = 5, projectileSpeed = 0} }
     };
 
-    public Dictionary<playerClass, playerStatPoints> currentStatPoints = new Dictionary<playerClass, playerStatPoints>()
+    public Dictionary<playerClass, playerStats> currentStatPoints = new Dictionary<playerClass, playerStats>()
     {
-        { playerClass.Barbarian, new playerStatPoints{ health = 1, defense = 1, attackPower = 1, attackSpeed = 1, moveSpeed = 1, projectileSpeed = 1} },
-        { playerClass.Mage, new playerStatPoints{ health = 1, defense = 1, attackPower = 1, attackSpeed = 1, moveSpeed = 1, projectileSpeed = 1} },
-        { playerClass.Archer, new playerStatPoints{ health = 1, defense = 1, attackPower = 1, attackSpeed = 1, moveSpeed = 1, projectileSpeed = 1} },
-        { playerClass.Paladin, new playerStatPoints{ health = 1, defense = 1, attackPower = 1, attackSpeed = 1, moveSpeed = 1, projectileSpeed = 1} }
+        { playerClass.Barbarian, new playerStats{ health = 1, defense = 1, attack = 1, attackSpeed = 1, moveSpeed = 1, projectileSpeed = 1} },
+        { playerClass.Mage, new playerStats{ health = 1, defense = 1, attack = 1, attackSpeed = 1, moveSpeed = 1, projectileSpeed = 1} },
+        { playerClass.Archer, new playerStats{ health = 1, defense = 1, attack = 1, attackSpeed = 1, moveSpeed = 1, projectileSpeed = 1} },
+        { playerClass.Paladin, new playerStats{ health = 1, defense = 1, attack = 1, attackSpeed = 1, moveSpeed = 1, projectileSpeed = 1} }
     };
     
     public Dictionary<playerClass, playerStatMultipliers> statMultipliers = new Dictionary<playerClass, playerStatMultipliers>()
     {
-        { playerClass.Barbarian, new playerStatMultipliers{ health = 1, defense = 1, attackPower = 1, attackSpeed = 1, moveSpeed = 1, projectileSpeed = 1} },
-        { playerClass.Mage, new playerStatMultipliers{ health = 1, defense = 1, attackPower = 1, attackSpeed = 1, moveSpeed = 1, projectileSpeed = 1} },
-        { playerClass.Archer, new playerStatMultipliers{ health = 1, defense = 1, attackPower = 1, attackSpeed = 1, moveSpeed = 1, projectileSpeed = 1} },
-        { playerClass.Paladin, new playerStatMultipliers{ health = 1, defense = 1, attackPower = 1, attackSpeed = 1, moveSpeed = 1, projectileSpeed = 1} }
+        { playerClass.Barbarian, new playerStatMultipliers{ health = 1f, defense = 1f, attackPower = 1f, attackSpeed = 1f, moveSpeed = 1f, projectileSpeed = 1f} },
+        { playerClass.Mage, new playerStatMultipliers{ health = 1f, defense = 1f, attackPower = 1f, attackSpeed = 1f, moveSpeed = 1f, projectileSpeed = 1f} },
+        { playerClass.Archer, new playerStatMultipliers{ health = 1f, defense = 1f, attackPower = 1f, attackSpeed = 1f, moveSpeed = 1f, projectileSpeed = 1f} },
+        { playerClass.Paladin, new playerStatMultipliers{ health = 1f, defense = 1f, attackPower = 1f, attackSpeed = 1f, moveSpeed = 1f, projectileSpeed = 1f} }
     };
 
     private void OnEnable()
