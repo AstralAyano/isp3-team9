a8c5c459889b7bb80085e8af6c8590216e14f494
K0d3y <125231413+K0d3y@users.noreply.github.com>
2023-08-21 12:49:54 +0800

    Update UISkillNode.cs

--- ISP3_TEAM9/Assets/Scripts/UI/UISkillNode.cs
+++ ISP3_TEAM9/Assets/Scripts/UI/UISkillNode.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class UISkillNode : MonoBehaviour
 {
+    [SerializeField]
+    private List<UISkillNode> adjacentNodes = new List<UISkillNode>();
+    private SpriteRenderer sr;
+
+    private bool isActivatable = false;
+    public bool isActivated = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
+        if (isActivated)
+        {
+            isActivatable = true;
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        for (int i = 0; i < adjacentNodes.Count; i++)
+        {
+            if (adjacentNodes[i].isActivated)
+            {
+                isActivatable = true;
+            }
+        }
+    }
+
+    public void ActivateNode()
+    {
+        if (isActivatable)
+        {
+            isActivated = true;
+        }
     }
 }
