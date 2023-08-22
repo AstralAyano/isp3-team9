f18ff62561c623b9180655664a06d379973c2aee
K0d3y <125231413+K0d3y@users.noreply.github.com>
2023-08-22 11:57:33 +0800

    added skill tree node button functionality

--- ISP3_TEAM9/Assets/Scripts/UI/UISkillNode.cs
+++ ISP3_TEAM9/Assets/Scripts/UI/UISkillNode.cs
@@ -1,36 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UISkillNode : MonoBehaviour
 {
     [SerializeField]
     private List<UISkillNode> adjacentNodes = new List<UISkillNode>();
-    private SpriteRenderer sr;
+    private Button btn;
 
+    [SerializeField]
     private bool isActivatable = false;
     public bool isActivated = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        sr = GetComponent<SpriteRenderer>();
+        btn = GetComponent<Button>();
+
         if (isActivated)
         {
-            isActivatable = true;
+            for (int i = 0; i < adjacentNodes.Count; i++)
+            {
+                adjacentNodes[i].isActivatable = true;
+            }
+            ColorBlock cb = btn.colors;
+            cb.normalColor = new Color32(150, 150, 150, 255);
+            cb.highlightedColor = new Color32(150, 150, 150, 255);
+            cb.selectedColor = new Color32(150, 150, 150, 255);
+            btn.colors = cb;
         }
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        for (int i = 0; i < adjacentNodes.Count; i++)
-        {
-            if (adjacentNodes[i].isActivated)
-            {
-                isActivatable = true;
-            }
-        }
+        
     }
 
     public void ActivateNode()
@@ -38,6 +43,15 @@ public class UISkillNode : MonoBehaviour
         if (isActivatable)
         {
             isActivated = true;
+            for (int i = 0; i < adjacentNodes.Count; i++)
+            {
+                adjacentNodes[i].isActivatable = true;
+            }
+            ColorBlock cb = btn.colors;
+            cb.normalColor = new Color32(150, 150, 150, 255);
+            cb.highlightedColor = new Color32(150, 150, 150, 255);
+            cb.selectedColor = new Color32(150, 150, 150, 255);
+            btn.colors = cb;
         }
     }
 }
