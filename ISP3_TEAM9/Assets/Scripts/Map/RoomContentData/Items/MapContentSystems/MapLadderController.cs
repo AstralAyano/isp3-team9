13eb688ce623c4dba4e275242c40c7063c18a675
K0d3y <125231413+K0d3y@users.noreply.github.com>
2023-08-23 16:59:54 +0800

    ladda

--- ISP3_TEAM9/Assets/Scripts/Map/RoomContentData/Items/MapContentSystems/MapLadderController.cs
+++ ISP3_TEAM9/Assets/Scripts/Map/RoomContentData/Items/MapContentSystems/MapLadderController.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class MapLadderController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private bool playerWithinRange = false;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.F) && playerWithinRange)
+        {
+            // swap scene
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerWithinRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerWithinRange = false;
+        }
     }
 }
