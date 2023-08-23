65ff909608e94a06047c169873ac72afe5dbd7a4
notJY <123806420+notJY@users.noreply.github.com>
2023-08-23 12:04:53 +0800

    Added stat changes to artefact scriptable objects

--- ISP3_TEAM9/Assets/Scripts/Inventory/Item.cs
+++ ISP3_TEAM9/Assets/Scripts/Inventory/Item.cs
@@ -19,6 +19,15 @@ public class Item : ScriptableObject
     public string itemName;
     [TextArea] public string itemDesc;
 
+    [Header("Stat Changes")]
+    public int health;
+    public int maxHealth;
+    public int attack;
+    public int defense;
+    public int attackSpeed;
+    public int moveSpeed;
+    public int projectileSpeed;
+
     //public ItemType type;
     //public ActionType actionType;
     //public Vector2Int range = new Vector2Int(5, 4);
