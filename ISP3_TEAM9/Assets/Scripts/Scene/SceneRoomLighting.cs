728f375b2a69c3666d899c700bf8ade584c2c31b
Bernard Ng <bernardngboonhao@gmail.com>
2023-08-23 10:46:36 +0800

    Added RoomLighting Prefab

---
+++ ISP3_TEAM9/Assets/Scripts/Scene/SceneRoomLighting.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class SceneRoomLighting : MonoBehaviour
+{
+    private Light2D thisLight;
+    private bool fadeIn, fadeOut;
+
+    void Awake()
+    {
+        thisLight = GetComponent<Light2D>();
+
+        fadeIn = false;
+        fadeOut = false;
+    }
+
+    void Update()
+    {
+        if (fadeIn && thisLight.intensity < 0.75f)
+        {
+            thisLight.intensity += Time.unscaledDeltaTime * 3;
+        }
+        else if (fadeIn && thisLight.intensity >= 0.75f)
+        {
+            fadeIn = false;
+        }
+
+        if (fadeOut && thisLight.intensity > 0f)
+        {
+            thisLight.intensity -= Time.unscaledDeltaTime * 3;
+        }
+        else if (fadeOut && thisLight.intensity <= 0f)
+        {
+            fadeOut = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        fadeIn = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        fadeOut = true;
+    }
+}
