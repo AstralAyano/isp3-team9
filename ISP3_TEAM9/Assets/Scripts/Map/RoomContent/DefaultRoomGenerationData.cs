c8208fa6e2699cc0ba6668939711675030ed73a0
K0d3y <125231413+K0d3y@users.noreply.github.com>
2023-08-23 16:35:45 +0800

    fixed dooooooooor

--- ISP3_TEAM9/Assets/Scripts/Map/RoomContent/DefaultRoomGenerationData.cs
+++ ISP3_TEAM9/Assets/Scripts/Map/RoomContent/DefaultRoomGenerationData.cs
@@ -11,7 +11,7 @@ public class DefaultRoomGenerationData : BaseRoomGenerationData
     [SerializeField]
     private MapRoomContentPrefabPlacer prefabPlacer;
 
-    public List<ContentPlacementData> chestPlacementData;
+    public List<EnemyPlacementData> enemyPlacementData;
     public List<ItemPlacementData> itemData;
 
     public override List<GameObject> ProcessRoom(Vector2Int roomCenter, HashSet<Vector2Int> roomFloor, HashSet<Vector2Int> roomFloorNoCorridors)
@@ -27,7 +27,7 @@ public class DefaultRoomGenerationData : BaseRoomGenerationData
         GameObject lightObject
             = prefabPlacer.CreateObject(lightSource, lightSpawnPoint + new Vector2(0.5f, 0.5f));
 
-        placedObjects.AddRange(prefabPlacer.PlaceContent(chestPlacementData, itemPlacementHelper));
+        placedObjects.AddRange(prefabPlacer.PlaceEnemies(enemyPlacementData, itemPlacementHelper));
 
         placedObjects.Add(lightObject);
 
