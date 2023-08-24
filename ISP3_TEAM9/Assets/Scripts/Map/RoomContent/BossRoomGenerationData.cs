83e5e34afc7537b746c8cc49951590f38fc4b9d6
K0d3y <125231413+K0d3y@users.noreply.github.com>
2023-08-24 13:16:22 +0800

    updated boss room stuff

---
+++ ISP3_TEAM9/Assets/Scripts/Map/RoomContent/BossRoomGenerationData.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossRoomGenerationData : BaseRoomGenerationData
+{
+    public GameObject lightSource;
+    public GameObject player;
+
+    public List<ItemPlacementData> itemData;
+
+    public List<EnemyPlacementData> enemyPlacementData;
+
+    [SerializeField]
+    private MapRoomContentPrefabPlacer prefabPlacer;
+
+    public override List<GameObject> ProcessRoom(
+        Vector2Int roomCenter,
+        HashSet<Vector2Int> roomFloor,
+        HashSet<Vector2Int> roomFloorNoCorridors)
+    {
+
+        ContentPlacementHelper itemPlacementHelper =
+            new ContentPlacementHelper(roomFloor, roomFloorNoCorridors);
+
+        List<GameObject> placedObjects =
+            prefabPlacer.PlaceAllItems(itemData, itemPlacementHelper);
+
+        Vector2Int lightSpawnPoint = roomCenter;
+
+        GameObject lightObject
+            = prefabPlacer.CreateObject(lightSource, lightSpawnPoint + new Vector2(0.5f, 0.5f));
+
+        placedObjects.Add(lightObject);
+
+        Vector2Int playerSpawnPoint = roomCenter;
+
+        GameObject playerObject
+            = prefabPlacer.CreateObject(player, playerSpawnPoint + new Vector2(0.5f, 0.5f));
+
+        placedObjects.Add(playerObject);
+
+        placedObjects.AddRange(prefabPlacer.PlaceEnemies(enemyPlacementData, itemPlacementHelper));
+
+        return placedObjects;
+    }
+}
