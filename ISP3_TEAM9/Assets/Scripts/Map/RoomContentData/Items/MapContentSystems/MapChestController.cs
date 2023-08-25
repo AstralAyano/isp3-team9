1786db0a79e4d4b10e756a3128006b9ce5ef04a6
K0d3y <125231413+K0d3y@users.noreply.github.com>
2023-08-25 15:39:28 +0800

    fixed boss level

--- ISP3_TEAM9/Assets/Scripts/Map/RoomContentData/Items/MapContentSystems/MapChestController.cs
+++ ISP3_TEAM9/Assets/Scripts/Map/RoomContentData/Items/MapContentSystems/MapChestController.cs
@@ -11,9 +11,17 @@ public class MapChestController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        if (Input.GetKeyDown(KeyCode.F) && !opened && playerWithinRange)
+        if (Input.GetKeyDown(KeyCode.F))
         {
+            OpenChest();
+        }
+    }
+
+    void OpenChest()
+    {
+        if (!opened && playerWithinRange)
+        {
+            Debug.Log("Chest Opened");
             opened = true;
             Instantiate(itemList.GetRandomItem(), transform.position, Quaternion.identity);
             Destroy(gameObject);
